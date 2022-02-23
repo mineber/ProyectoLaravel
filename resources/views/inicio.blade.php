d9fc311a3beff04ec80e98ba20e8ba6d299cb619
tecnicolopeza <tecnicolopeza@gmail.com>
2022-02-23 13:51:23 +0100

    copiadoWhtsp

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,18 +9,12 @@
             <div class="card-juego">
                 <img src="{{asset('img/'.$juego->imagen)}}" alt="">
                 <div class="cuerpo-card">
-<<<<<<< HEAD
-                    <h4>{{$juego->nombre_juego}}</h4>
-                    <p>Vendedor: </p>
-=======
                     <a href="">{{$juego->nombre_juego}}</a>
                     <p>Vendedor: @foreach($users as $user)
                         @if ($user->id == $juego->user_id)
                             {{$user->nombre_usuario}}
                         @endif
                     @endforeach</p>
-                     
->>>>>>> 6f9d9faf402b0207db786768e8357e5515bfb26a
                     {{substr($juego->descripcion, 0, 20)}}
                     <p>Precio: {{$juego->precio}} €</p>
                     <a href="#" class="boton-comprar">comprar</a>
