3a13f9035509045514558f7ad54177d0046923f4
tecnicolopeza <tecnicolopeza@gmail.com>
2022-02-25 17:58:03 +0100

    iconos

--- resources/views/layouts/plantillaJuegos.blade.php
+++ resources/views/layouts/plantillaJuegos.blade.php
@@ -5,10 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/styleJuegos.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
-    <link href='iconos\boxicons-2.1.1\css\boxicons.min.css' rel='stylesheet'>
+    <link href="{{asset('icons/boxicons-2.1.1/css/boxicons.min.css')}}" rel='stylesheet'>
     <title>@yield('title')</title>
 </head>
 
@@ -24,12 +22,12 @@
             <a href="{{route('plataforma','pc')}}" class="caja">PC</a>
         </div>
         @if (isset($_SESSION['usuario']))
-           <div class="banner-right">
+        <div class="banner-right">
             <a href="{{route('login')}}" class="caja IS">{{$_SESSION['usuario']}}</a>
             <a href="{{route('cs')}}" class="caja IS">Cerrar Sesión</a>
             <a href="#" class="caja CC">Carro</a>            
         </div>
-           @else
+            @else
         <div class="banner-right">
             <a href="{{route('login')}}" class="caja IS">Inicia sesión</a>
             <a href="#" class="caja CC">Crear cuenta</a>            
